6c87220bf1773ccd483114b3b340c0cf74cd0c40
Egu Chinedu <eguchinedu18@gmail.com>
2023-07-13 03:12:19 +0100

    deployed to firebase

---
+++ src/app/config/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2d9qFLmKJMIQJKXtKlQ1gk21Uv6n3IvI',
+  authDomain: 'moniepassweb.firebaseapp.com',
+  projectId: 'moniepassweb',
+  storageBucket: 'moniepassweb.appspot.com',
+  messagingSenderId: '87681054582',
+  appId: '1:87681054582:web:c7fe7dac9426d10812ac57',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
